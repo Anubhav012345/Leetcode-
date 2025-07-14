d6caa138b98b22e0654574753214de1c8c83c77d
Anubhav012345 <149472250+Anubhav012345@users.noreply.github.com>
2025-07-14 10:43:15 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (15.78%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # visit=set()
+        ans=[]
+        def dfs(path):
+            if len(path)==len(nums):
+                ans.append(path)
+                return
+            for i in nums:
+                if i not in path:
+                    dfs(path+[i])
+        dfs([])
+        return ans
+            
