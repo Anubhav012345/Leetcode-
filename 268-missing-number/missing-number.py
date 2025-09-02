830163e49bb61bfeacfa9e6404bc3bbb2f3c4a5a
Anubhav012345 <149472250+Anubhav012345@users.noreply.github.com>
2025-09-02 12:13:16 +0530

    Time: 12 ms (21.77%) | Memory: 18.9 MB (16.89%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,6 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        freq={}
         n=len(nums)
-        for i in range(n+1):
-            if i not in nums:
-                return i
+        for i in range(0,n+1):
+            freq[i]=0
+        for num in nums:
+            freq[num]=1
+        for k,v in freq.items():
+            if v==0:
+                return k
+
+        # Brute Force
+        # n=len(nums)
+        # for i in range(n+1):
+        #     if i not in nums:
+        #         return i
