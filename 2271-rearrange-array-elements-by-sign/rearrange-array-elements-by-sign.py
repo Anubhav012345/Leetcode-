2d26ae74c7354efb0b8027637a20cbbe4669de03
Anubhav012345 <149472250+Anubhav012345@users.noreply.github.com>
2025-09-04 11:27:21 +0530

    Time: 62 ms (27.57%) | Memory: 43 MB (30.79%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -2,6 +2,7 @@ class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         lst=[]
         lst1=[]
+        res=[]
         for i in range(len(nums)):
             if(nums[i]>0):
                 lst.append(nums[i])
@@ -11,8 +12,11 @@ class Solution:
         # for i,j in zip(lst,lst1):
         #     merged.append(lst)
         #     merged.append(lst1)
-        merged = [x for pair in zip(lst, lst1) for x in pair]
-        return merged
+        # merged = [x for pair in zip(lst, lst1) for x in pair]
+        for i in range(len(lst)):
+            res.append(lst[i])
+            res.append(lst1[i])
+        return res
 
 
 
