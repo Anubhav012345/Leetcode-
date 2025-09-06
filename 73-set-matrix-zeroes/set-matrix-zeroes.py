c16e1b013da67156a6b328da470be5fd4159ce81
Anubhav012345 <149472250+Anubhav012345@users.noreply.github.com>
2025-09-06 22:39:39 +0530

    Time: 1 ms (81.69%) | Memory: 18.6 MB (38.23%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,28 +1,20 @@
 class Solution:
-    def mark_infinity(self,matrix,row,column):
-        r=len(matrix)
-        col=len(matrix[0])
-        for i in range(r):
-            if matrix[i][column]!=0:
-                matrix[i][column]=float("inf")
-        for j in range(col):
-            if (matrix[row][j]!=0):
-                matrix[row][j]=float("inf")
-
-
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows=len(matrix)
-        col=len(matrix[0])
-        for i in range(rows):
-            for j in range(col):
+        r=len(matrix)
+        c=len(matrix[0])
+        row_track=[0]*r
+        col_track=([0]*(c))
+        for i in range(r):
+            for j in range(c):
                 if(matrix[i][j]==0):
-                    self.mark_infinity(matrix,i,j)
-        for i in range(rows):
-            for j in range(col):
-                if matrix[i][j]==float("inf"):
+                    row_track[i]=-1
+                    col_track[j]=-1
+        for i in range(r):
+            for j in range(c):
+                if(row_track[i]==-1 or col_track[j]==-1):
                     matrix[i][j]=0
         return matrix
         
