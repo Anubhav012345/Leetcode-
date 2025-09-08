07080f6298fa2bdf09f84577f407cd3c5ec35691
Anubhav012345 <149472250+Anubhav012345@users.noreply.github.com>
2025-09-08 23:22:08 +0530

    Time: 4 ms (99.72%) | Memory: 32.3 MB (67.54%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,9 +1,8 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        seen = {}
-        for i, val in enumerate(nums):
-            if val in seen and i - seen[val] <= k:
+        if k == 0 or len(nums) == len(set(nums)):
+            return False
+        for i in range(len(nums)):
+            if nums[i] in nums[i + 1 : i + k + 1]:
                 return True
-            else:
-                seen[val] = i
         return False
