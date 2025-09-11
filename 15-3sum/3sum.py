8e75ba8c2d8da912ace4ef42f09142d01972346f
Anubhav012345 <149472250+Anubhav012345@users.noreply.github.com>
2025-09-11 13:23:29 +0530

    Time: 575 ms (59.77%) | Memory: 20.4 MB (91.55%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,59 +1,31 @@
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         ans=[]
-#         nums.sort()
-#         n=len(nums)
-#         for i in range(n):
-#             if i!=0 and nums[i]==nums[i-1]:
-#                 continue
-#             # moving two pointers
-#             j=i+1
-#             k=n-1
-#             while j<k:
-#                 total_sum=nums[i]+nums[j]+nums[k]
-#                 if total_sum<0:
-#                     j+=1
-#                 elif(total_sum>0):
-#                     k-=1
-#                 else:
-#                     ans.append([nums[i],nums[j],nums[k]])
-#                     j+=1
-#                     k-=1
-#                 while j<k and nums[j]==nums[j-1]:
-#                     j+=1
-#                 # if j<k and k==n:
-#                 #     break
-#                 #     while j<k and nums[k]==nums[k+1]:    
-#                 #         k-=1
-#         return ans
-            
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        ans=[]
         nums.sort()
-
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
+        n=len(nums)
+        for i in range(n):
+            if i!=0 and nums[i]==nums[i-1]:
                 continue
-            
-            j = i + 1
-            k = len(nums) - 1
-
-            while j < k:
-                total = nums[i] + nums[j] + nums[k]
-
-                if total > 0:
-                    k -= 1
-                elif total < 0:
-                    j += 1
+            # moving two pointers
+            j=i+1
+            k=n-1
+            while j<k:
+                total_sum=nums[i]+nums[j]+nums[k]
+                if total_sum<0:
+                    j+=1
+                elif(total_sum>0):
+                    k-=1
                 else:
-                    res.append([nums[i], nums[j], nums[k]])
-                    j += 1
+                    ans.append([nums[i],nums[j],nums[k]])
+                    j+=1
+                    k-=1
+                    while j<k and nums[j]==nums[j-1]:
+                        j+=1
+                    while j<k and nums[k]==nums[k+1]:    
+                        k-=1
+        return ans
+            
 
-                    while nums[j] == nums[j-1] and j < k:
-                        j += 1
-        
-        return res
 
 
         
