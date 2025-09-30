e3113332db7205acba81698d82187b508d275582
Anubhav012345 <149472250+Anubhav012345@users.noreply.github.com>
2025-09-30 16:10:13 +0530

    Time: 16 ms (39.73%) | Memory: 18.1 MB (96.13%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,40 +1,27 @@
 class Solution:
-    def safe(self,row,col,board,n):
-        duprow=row
-        dupcol=col
-        while row>=0 and col>=0:
-            if board[row][col]=="Q":
-                return False
-            row-=1
-            col-=1
-        row=duprow
-        col=dupcol
-        while col>=0:
-            if board[row][col]=="Q":
-                return False
-            col-=1
-        row=duprow
-        col=dupcol
-        while row<n and col>=0:
-            if board[row][col]=="Q":
-                return False
-            col-=1
-            row+=1
-        return True
-    
-    def solve(self,col,board,ans,n):
+    def solve(self,col,board,leftrow,lowerdiagonal,upperdiagonal,ans,n):
         if col==n:
             ans.append(board.copy())
             return
-        for j in range(n):
-            if self.safe(j,col,board,n):
-                board[j]=board[j][:col]+"Q"+board[j][col+1:]
-                self.solve(col+1,board,ans,n)
-                board[j]=board[j][:col]+"."+board[j][col+1:]                
-
+        for row in range(n):
+            low_diagonal=row+col
+            up_diagonal=n-1+col-row
+            if(leftrow[row]==0 and lowerdiagonal[low_diagonal]==0 and upperdiagonal[up_diagonal]==0):
+                board[row]=board[row][:col]+ "Q" +board[row][col+1:]
+                leftrow[row]=1
+                lowerdiagonal[low_diagonal]=1
+                upperdiagonal[up_diagonal]=1
+                self.solve(col+1,board,leftrow,lowerdiagonal,upperdiagonal,ans,n)
+                board[row]=board[row][:col]+"."+board[row][col+1:]
+                leftrow[row]=0
+                lowerdiagonal[low_diagonal]=0
+                upperdiagonal[up_diagonal]=0                      
     def solveNQueens(self, n: int) -> List[List[str]]:
         ans=[]
         board=["."*n for _ in range(n)]
-        self.solve(0,board,ans,n)
+        leftrow=[0]*n
+        lowerdiagonal=[0]*(2*n-1)
+        upperdiagonal=[0]*(2*n-1)
+        self.solve(0,board,leftrow,lowerdiagonal,upperdiagonal,ans,n)
         return ans
         
