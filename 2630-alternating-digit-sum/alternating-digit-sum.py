afe5943679f103c8b60d865a432aad5d14ffe742
Anubhav012345 <149472250+Anubhav012345@users.noreply.github.com>
2025-10-06 16:32:59 +0530

    Time: 3 ms (3.43%) | Memory: 17.8 MB (61.04%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.py
+++ 2630-alternating-digit-sum/alternating-digit-sum.py
@@ -1,14 +1,12 @@
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
-        odd_sum=0
         even_sum=0
         digits=[int(d) for d in str(n)]
         for i in range(0,len(digits)):
             if i%2==0:
                 even_sum+=digits[i]
             else:
-                odd_sum+=digits[i]
-        ans=even_sum-odd_sum
-        return ans
+                even_sum-=digits[i]
+        return even_sum
 
         
