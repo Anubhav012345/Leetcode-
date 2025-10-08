41f15e972843aef788aa760c4c28f3dd63b311ed
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2025-10-08 22:49:52 +0530

    Time: 9 ms (43.82%) | Memory: 17.8 MB (64.36%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,17 +1,15 @@
 class Solution:
-    def func(self,x,left,right):
-        if(int(x)<0):
-            return False
-        if(left>=right):
-            return True
-        if(x[left]!=x[right]):
-            return False
-        return self.func(x,left+1,right-1)
     def isPalindrome(self, x: int) -> bool:
-        x=str(x)
-        left=0
-        right=len(x)-1
-        return self.func(x,left,right)
+        if(x<0):
+            return False
+        num=x
+        result=0
+        while num>0:
+            lastdigit=num%10
+            result=result*10+lastdigit
+            num=num//10
+        return x==result
+
 
 
 
