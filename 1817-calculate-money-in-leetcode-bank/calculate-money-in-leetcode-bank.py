d2bcf5d30a7e208149fcda8b93306c2924da0054
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2025-10-25 18:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (89.38%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -1,13 +1,13 @@
 class Solution:
-    def solve(self,i,total,n,count):
-        if(count==n):
-            return total
-        total+=i
-        if(count+1)%7==0:
-            next_week=i-(count%7)+1
-        else:
-            next_week=i+1
-        return self.solve(next_week,total,n,count+1)
     def totalMoney(self, n: int) -> int:
-        return self.solve(1,0,n,0)
+        total=0
+        week_start=1
+        day=0
+        for _ in range(n):
+            total+=week_start+day
+            day+=1
+            if(day==7):
+                week_start+=1
+                day=0
+        return total
         
