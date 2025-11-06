b4621af5733e9b47ca7a84cf1843b5b040fd63c6
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2025-11-06 13:42:07 +0530

    Time: 43 ms (27.57%) | Memory: 36.1 MB (23.10%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        hash_map={}
+        for i in range(len(nums)):
+            hash_map[nums[i]]=hash_map.get(nums[i],0)+1
+        lst=[]
+        for k,v in hash_map.items():
+            if v>1:
+                lst.append(k)
+        return lst
+        
