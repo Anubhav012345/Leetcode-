2e6dae61e414f48651042724a75012827857164a
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2025-11-07 16:40:09 +0530

    Time: 7 ms (52.61%) | Memory: 17.9 MB (42.19%) - LeetSync

--- 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.py
+++ 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.py
@@ -1,20 +1,7 @@
 class Solution:
     def smallestAbsent(self, nums: List[int]) -> int:
-        n=len(nums)
-        if(n==1):
-            if(nums[0]>=0):
-                return nums[0]+1
-            else:
-                return 1
-        else:
-            x=sum(nums)/n
-            if x<0:
-                ans=1
-                while ans in nums:
-                    ans+=1
-                return ans
-            ans=int(x)+1
-            while ans in nums:
-                ans+=1
-            return ans
-        
+        avg=sum(nums)//len(nums)
+        i=max(0, avg) + 1
+        while i in nums:
+            i+=1
+        return i
