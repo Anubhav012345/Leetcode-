1f9f9d9caad8193b4590c85dcd71cc2626c5cc10
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2025-11-07 12:38:27 +0530

    Time: 79 ms (5.23%) | Memory: 17.8 MB (38.36%) - LeetSync

--- 4052-equal-score-substrings/equal-score-substrings.py
+++ 4052-equal-score-substrings/equal-score-substrings.py
@@ -3,20 +3,20 @@ class Solution:
         s = s.lower().strip()
         n = len(s)
 
-        for k in range(1, n):  # try every possible split position
+        for k in range(1, n):  
             string1 = s[:k]
             string2 = s[k:]
 
             sum1 = 0
             for i in range(len(string1)):
                 ascii_val = ord(string1[i])
-                score1 = ascii_val - 97 + 1
+                score1 = ascii_val - 97 + 1   
                 sum1 += score1
 
             sum2 = 0
             for j in range(len(string2)):
                 ascii_value1 = ord(string2[j])
-                score2 = ascii_value1 - 97 + 1
+                score2 = ascii_value1 - 97 + 1   
                 sum2 += score2
 
             if sum1 == sum2:
