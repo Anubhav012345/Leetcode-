fb61ab9d8914ff01be814769d0f7d7394697dcaf
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2025-11-21 11:45:08 +0530

    Time: 3 ms (27.09%) | Memory: 17.7 MB (87.05%) - LeetSync

--- 4039-compute-decimal-representation/compute-decimal-representation.py
+++ 4039-compute-decimal-representation/compute-decimal-representation.py
@@ -1,16 +1,12 @@
 class Solution:
     def decimalRepresentation(self, n: int) -> List[int]:
-        result=[]
-        count=0
-        while n!=0:
-            rem=n%10
-            mul=10**count
-            result.append(rem*mul)
-            n//=10
-            count+=1
-        result.reverse()
-        ans=[]
-        for i in range(len(result)):
-            if result[i]!=0:
-                ans.append(result[i])
-        return ans
+        ans = []
+        factor = 1
+        parts = []
+        while n > 0:
+            rem = n % 10
+            if rem != 0:
+                parts.append(rem * factor)
+            n //= 10
+            factor *= 10
+        return parts[::-1]
