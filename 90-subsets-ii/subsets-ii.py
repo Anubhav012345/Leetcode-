9d0a2940482857fc0da4e0d8a7296f49790d914a
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2025-12-02 22:21:57 +0530

    Time: 1 ms (30.56%) | Memory: 18 MB (35.78%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,15 +1,14 @@
 class Solution:
-    def solve(self, index, n, nums, subset):
-        if index >= n:
-            return [subset.copy()]      
-        results = []
-        subset.append(nums[index])
-        results += self.solve(index + 1, n, nums, subset)
-        subset.pop()
-        while index + 1 < n and nums[index] == nums[index + 1]:
-            index += 1
-        results += self.solve(index + 1, n, nums, subset)
-        return results
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        return self.solve(0, len(nums), nums, [])
+        res = []
+        def backtrack(start, subset):
+            res.append(subset.copy())
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                subset.append(nums[i])
+                backtrack(i + 1, subset)
+                subset.pop()
+        backtrack(0, [])
+        return res
