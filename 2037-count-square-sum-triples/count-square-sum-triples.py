b35fb7785b90ff87edab28198c2d07719ff6d4b0
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2025-12-08 09:54:38 +0530

    Time: 266 ms (38.05%) | Memory: 17.7 MB (80.71%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -0,0 +1,16 @@
+from math import sqrt 
+class Solution:
+    def countTriples(self, n: int) -> int:
+        count=0
+        for a in range(1,n+1):
+            for b in range(1,n+1):
+                c=int(sqrt((a*a)+(b*b)))
+                if(c<=n and c*c==(a*a)+(b*b)):
+                    count+=1
+        return count
+
+
+
+
+
+        
