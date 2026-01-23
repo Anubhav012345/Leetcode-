c638a71971de9910164fd0a53c3dad02745abe14
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2026-01-23 22:53:49 +0530

    Time: 4 ms (9.97%) | Memory: 19.4 MB (49.00%) - LeetSync

--- 4178-vowel-consonant-score/vowel-consonant-score.py
+++ 4178-vowel-consonant-score/vowel-consonant-score.py
@@ -1,21 +1,15 @@
 class Solution:
     def vowelConsonantScore(self, s: str) -> int:
-        vowels='aeiou'
-        number='0123456789'
-        vowel_count=0
-        consonants=0
+        vowels = 'aeiou'
+        c = 0
+        v = 0
+        for ch in s:
+            if 'a' <= ch and ch <= 'z':
+                if ch in vowels:
+                    v += 1
+                else:
+                    c += 1
+        return 0 if 0 == c else floor(v / c)
 
-        for i in range(len(s)):
-            if s[i] in vowels:
-                vowel_count+=1
-            elif s[i] in number:
-                continue
-            elif s[i]==' ':
-                continue
-            else:
-                consonants+=1        
-        if(consonants==0):
-            return 0
-        return(vowel_count//consonants)
 
         
