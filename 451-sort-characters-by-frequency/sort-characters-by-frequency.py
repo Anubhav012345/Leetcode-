588c972f38db93427acb8e9c17972f2f0fb94b90
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2026-02-02 11:13:43 +0530

    Time: 10 ms (57.87%) | Memory: 20.2 MB (43.71%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -0,0 +1,16 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        hash_map={}
+        for i in range(len(s)):
+            hash_map[s[i]]=hash_map.get(s[i],0)+1
+
+        def get_value(item):
+            return item[1]
+
+        items=sorted(hash_map.items(),key=get_value,reverse=True)
+        
+        result=""
+        for key,value in items:
+            result+=key*value
+
+        return result        
