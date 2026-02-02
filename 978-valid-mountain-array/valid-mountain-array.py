7efb91be868f8040a97d35e62cae3d78d2e3ada0
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2026-02-02 12:10:50 +0530

    Time: 162 ms (50.43%) | Memory: 20.6 MB (20.73%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        n=len(arr)
+        
+        if(n<3):
+            return False
+        
+        peak=-1
+
+        for i in range(len(arr)-1):
+            if(arr[i]==arr[i+1]):
+                return False
+            if(arr[i]>arr[i+1]):
+                peak=i
+                break
+        
+        if(peak<=0 or peak>=n-1):
+            return False
+
+        for i in range(peak):
+            if(arr[i]>=arr[i+1]):
+                return False
+        
+        for i in range(peak,n-1):
+            if(arr[i]<=arr[i+1]):
+                return False
+
+        return True
+        
