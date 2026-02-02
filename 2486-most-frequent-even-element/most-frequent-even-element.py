1cc93ba731930d336dfe9e1fcf8d1ef28e746d70
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2026-02-02 11:34:52 +0530

    Time: 46 ms (13.12%) | Memory: 19.7 MB (16.13%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        nums.sort()
+        hash_map={}
+        for i in range(len(nums)):
+            if(nums[i]%2==0):
+                hash_map[nums[i]]=hash_map.get(nums[i],0)+1
+        
+        maxi=float("-inf")    
+        for key, value in hash_map.items():
+            maxi=max(maxi,value)
+
+        ans=[]
+        for key,value in hash_map.items():
+            if(value==maxi):
+                ans.append(key)
+        if(len(ans)==0):
+            return -1
+        return(ans[0])
+        
