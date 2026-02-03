01c74327c8a0b13d6e996fb4bec37ddeb8896962
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2026-02-03 10:44:05 +0530

    Time: 788 ms (27.43%) | Memory: 19.5 MB (48.55%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.py
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.py
@@ -2,19 +2,12 @@ class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
         pairs.sort()
         n=len(pairs)
-        memo={}
-        ans=0
-        for i in range(n):
-            ans=max(ans,self.solve(i,n,pairs,memo))
-        return ans
+        dp=[1]*n
 
-    def solve(self,x,n,pairs,memo):
-        if x in memo:
-            return memo[x]
-        temp_ans=0
-        for j in range(x+1,n):
-            if(pairs[x][1]<pairs[j][0]):
-                temp_ans=max(temp_ans,self.solve(j,n,pairs,memo))
-        memo[x]=1+temp_ans
-        return 1+temp_ans
+        for i in range(n-1,-1,-1):
+            for j in range(i+1,n):
+                if(pairs[i][1]<pairs[j][0]):
+                    dp[i]=max(dp[i],dp[j]+1)
+                
+        return max(dp)
         
