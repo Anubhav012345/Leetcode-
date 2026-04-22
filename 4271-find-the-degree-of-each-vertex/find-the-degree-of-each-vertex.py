72368118ace27567b699ec57866ca9da409c961f
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2026-04-22 23:00:37 +0530

    Time: 33 ms (6.78%) | Memory: 20 MB (33.74%) - LeetSync

---
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findDegrees(self, matrix: list[list[int]]) -> list[int]:
+
+        n = len(matrix)
+        result = [0] * n
+
+        for i in range(n):
+            for j in range(n):
+                if matrix[i][j] == 1 and i < j:
+                    result[i] += 1
+                    result[j] += 1
+
+        return result
