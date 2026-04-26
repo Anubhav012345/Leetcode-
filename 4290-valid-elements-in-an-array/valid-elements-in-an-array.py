3c5555f0afda98d24e6b96bef90745927ffcc30c
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2026-04-26 21:28:28 +0530

    Time: 6 ms (37.29%) | Memory: 19.2 MB (88.05%) - LeetSync

--- 4290-valid-elements-in-an-array/valid-elements-in-an-array.py
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.py
@@ -1,22 +1,22 @@
 class Solution:
     def findValidElements(self, nums: list[int]) -> list[int]:
-        n=len(nums)
-        leftgreat=[0]*n
-        rightgreat=[0]*n
+        ans=[]
+        mx=0
+        for i in range(len(nums)):
+            if nums[i]>mx:
+                mx=nums[i]
+                ans.append(i)
+
+        mx=0
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i]>mx:
+                mx=nums[i]
+                ans.append(i)
+        ans=list(set(ans))
+        ans.sort()
         res=[]
-        if n==1:
-            return[nums[0]]
-        for i in range(n):
-            if i==0:
-                leftgreat[i]=0
-            else:
-                leftgreat[i]=max(nums[i-1],leftgreat[i-1])
-            j=n-i-1
-            if j==n-1:
-                rightgreat[j]=0
-            else:
-                rightgreat[j]=max(nums[j+1],rightgreat[j+1])
-        for i in range(n):
-            if i==0 or i==n-1 or nums[i]>leftgreat[i] or nums[i]>rightgreat[i]:
-                res.append(nums[i])
+        for i in ans:
+            res.append(nums[i])
+
         return res
+                
