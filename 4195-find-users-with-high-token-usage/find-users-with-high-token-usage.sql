9f238308c04e7e1837f479dbf855b6d2118dd795
Anubhav Srivastava <149472250+Anubhav012345@users.noreply.github.com>
2026-04-27 23:36:37 +0530

    Time: 295 ms (38.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 4195-find-users-with-high-token-usage/find-users-with-high-token-usage.sql
@@ -0,0 +1,8 @@
+SELECT
+    user_id, 
+    count(prompt) as prompt_count,
+    ROUND(avg(tokens),2) as avg_tokens
+FROM prompts 
+GROUP BY user_id
+HAVING COUNT(prompt) > 2 AND MAX(tokens) > AVG(tokens) 
+ORDER BY avg(tokens) desc, user_id;
